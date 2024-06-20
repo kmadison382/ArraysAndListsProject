cdadaa4250f28c385fa35bf1e45ac3cc4a696f0a
kmadison382 <k.madison.382@gmail.com>
2024-06-19 20:29:10 -0500

    even or not

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,17 +10,18 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
+            int[] oneToTen = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
             /* Create a list of type int
              * Name the list "evens"
              */
+             List<int> evens = new List<int>();
 
             
             /* Create another list of type int
              * Name the list "odds"
              */
-            
+            List<int> odds = new List<int>();
 
              /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
@@ -28,17 +30,36 @@ namespace ArraysAndLists
              * If the number is even, add it to the evens list.
              * If the number is odd, add it to the odds list.
              */
+             foreach (int digit in oneToTen)
+             {
+                if (digit % 2 == 0)
+                {
+                    evens.Add(digit);
+                }
+                else
+                {
+                    odds.Add(digit);
+                }
+             }
 
 
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
-            
+            foreach (int evenNum in evens)
+            {
+                Console.WriteLine(evenNum);
+            }
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+             for (int i = 0; i < odds.Count; i++)
+             {
+                Console.WriteLine(odds[i]);
+             }
+             
         }
     }
 }
